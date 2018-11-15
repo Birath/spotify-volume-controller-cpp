dc156e3068f42ec4be3a12e7f4b2dd749d3223a0
Björn Birath <bjorn.birath@gmail.com>
2018-11-15 23:57:37 +0100

    Added member to authorize header

--- Client.cpp
+++ Client.cpp
@@ -1,28 +1,45 @@
 #include "Client.h"
+#include "HttpUtils.h"
 
-Client::Client(json::value &token_info) : m_token_info(token_info) {};
+Client::Client(json::value &token_info) : m_token_info(token_info), BASE_API_URI(L"https://api.spotify.com") {};
 
 
-Client::~Client()
-{
+Client::~Client() {
 }
 
 pplx::task<http::http_response> Client::api_request(const utility::string_t & endpoint, const http::method http_method) {
-	uri_builder api_uri(Config::BASE_API_URI);
-	api_uri.append_path(endpoint);
+	http::http_request request;
+	request.set_request_uri(endpoint);
+	request.set_method(http_method);
+	authorize_header(request);
+
+	uri_builder api_uri(BASE_API_URI);
 	http::client::http_client client(api_uri.to_uri());
-	return client.request(http_method).then([](http::http_response response) {
+
+	return client.request(request).then([](http::http_response response) {
 		return response;
 	});
 }
 
 pplx::task<http::http_response> Client::api_request(const utility::string_t & endpoint, const json::value data, const http::method http_method) {
-	uri_builder api_uri(Config::BASE_API_URI);
+	uri_builder api_uri(BASE_API_URI);
 	api_uri.append_path(endpoint);
 	api_uri.append_query(data.serialize());
 	std::wcout << data.serialize() << std::endl;
 	//utility::conversions::print_string(api_uri.to_string());
 	return api_request(api_uri.to_string(), http_method);
+}
+
+pplx::task<json::value> Client::get_device_info() {
+	return api_request(L"/v1/me/player/devices", http::methods::GET).then([](http::http_response response) {
+		std::wcout << get_json_response_body(response) << std::endl;
+		return response.extract_json();
+	});
+}
+
+void Client::authorize_header(http::http_request request) {
+	//request.headers().add(L"Authorization", L"Bearer " + m_token_info[ACCESS_TOKEN].serialize());
+	request.headers()[L"Authorization"] = L"Bearer " + m_token_info[ACCESS_TOKEN].as_string();
 }
 
 
--- Client.h
+++ Client.h
@@ -10,11 +10,22 @@ class Client
 public:
 	Client(json::value &token_info);
 	~Client();
-
+	/// <summary>
+	/// Makes a http request to *endpoint* using http method *http_method*
+	/// </summary>
 	pplx::task<http::http_response> api_request(const utility::string_t &endpoint, const http::method http_method);
 	pplx::task<http::http_response> api_request(const utility::string_t &endpoint, const json::value data, const http::method http_method);
+	/// <summary>
+	/// Makes a request to the device endpoint
+	/// </summary>
+	pplx::task<json::value> get_device_info();
 
 private:
+
+	void authorize_header(http::http_request request);
+
 	json::value m_token_info;
+	const utility::string_t ACCESS_TOKEN = L"access_token";
+	const uri BASE_API_URI;
 };
 
