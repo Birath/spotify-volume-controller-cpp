ab30eb5507cf39c672ef2b578b94628c44eb6f0a
Björn Birath <bjorn.birath@gmail.com>
2019-03-14 21:20:05 +0100

    change functions to not return tasks

--- Client.cpp
+++ Client.cpp
@@ -1,6 +1,8 @@
 #include "Client.h"
 #include "http_utils.h"
 #include "oauth.h"
+
+#include <boost/algorithm/string.hpp>
 const uri Client::BASE_API_URI(L"https://api.spotify.com");
 
 Client::Client(json::value &token_info, const Config &config) : m_token_info(token_info), m_config(config)  {}
@@ -8,20 +10,17 @@ Client::Client(json::value &token_info, const Config &config) : m_token_info(tok
 Client::~Client () {
 }
 
-pplx::task<http::http_response> Client::api_request(const utility::string_t & endpoint, const http::method http_method) {
+http::http_response Client::api_request(const utility::string_t & endpoint, const http::method http_method) {
 	http::http_request request;
 	request.set_request_uri(endpoint);
 	request.set_method(http_method);
 	authorize_header(request);
 	uri_builder api_uri(BASE_API_URI);
 	http::client::http_client client(api_uri.to_uri());
-
-	return client.request(request).then([](http::http_response response) {
-		return response;
-	});
+	return client.request(request).get();
 }
 
-pplx::task<http::http_response> Client::api_request(const utility::string_t & endpoint, const http::method http_method, const utility::string_t &query) {
+http::http_response Client::api_request(const utility::string_t & endpoint, const http::method http_method, const utility::string_t &query) {
 	uri_builder api_uri(BASE_API_URI);
 	api_uri.append_path(endpoint);
 	api_uri.append_query(query, true);
@@ -29,10 +28,22 @@ pplx::task<http::http_response> Client::api_request(const utility::string_t & en
 	return api_request(api_uri.to_string(), http_method);
 }
 
-pplx::task<json::value> Client::get_device_info() {
-	return api_request(L"/v1/me/player/devices", http::methods::GET).then([](http::http_response response) {
+json::value Client::get_device_info() {
+	try {
+		http::http_response response = api_request(L"/v1/me/player/devices", http::methods::GET);
 		return get_json_response_body(response).get();
-	});
+	}
+	catch (http::http_exception e) {
+		utility::stringstream_t error;
+		std::wcout << "Spotify API error:" << std::endl;
+		std::string msg = e.error_code().message();
+		boost::trim(msg);
+		//msg.erase(std::remove_if(msg.begin(), msg.end(), std), msg.end());
+
+		std::cout << msg.length();
+		
+		return json::value::null();
+	}
 }
 
 bool Client::set_device_volume(int volume, const utility::string_t &device_id) {
@@ -44,20 +55,18 @@ bool Client::set_device_volume(int volume, const utility::string_t &device_id) {
 			query << "device_id=" << device_id;
 		}
 
-		api_request(L"/v1/me/player/volume", http::methods::PUT, query.str()).then([=](http::http_response response) {
-			if (response.status_code() == http::status_codes::NoContent) {
-				return true;
-			}
-			else if (response.status_code() == http::status_codes::NotFound || response.status_code() == http::status_codes::Forbidden) {
-				print_play_error_message(response);
-				return false;
-			}
-			else {
-				print_error_message(response);
-				return false;
-			}
-		});
-		
+		http::http_response response = api_request(L"/v1/me/player/volume", http::methods::PUT, query.str());
+		if (response.status_code() == http::status_codes::NoContent) {
+			return true;
+		}
+		else if (response.status_code() == http::status_codes::NotFound || response.status_code() == http::status_codes::Forbidden) {
+			print_play_error_message(response);
+			return false;
+		}
+		else {
+			print_error_message(response);
+			return false;
+		}
 	}
 	return false;
 	
@@ -68,7 +77,12 @@ bool Client::set_volume(int volume) {
 }
 
 int Client::get_current_playing_volume() {
-	json::array devices = get_device_info().get()[L"devices"].as_array();
+	using namespace json;
+	value device_info = get_device_info();
+	if (device_info.is_null()) {
+		return -1;
+	}
+	array devices = device_info[L"devices"].as_array();
 
 	for each (auto device in devices) {
 		if (device[L"is_active"].as_bool()) {
@@ -80,7 +94,7 @@ int Client::get_current_playing_volume() {
 }
 
 json::value Client::get_desktop_player() {
-	json::array devices = get_device_info().get()[L"devices"].as_array();
+	json::array devices = get_device_info()[L"devices"].as_array();
 
 	for each (auto device in devices) {
 		if (device[L"type"].as_string().compare(L"Computer") == 0) return device;
